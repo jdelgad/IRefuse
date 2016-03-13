222dde91d3d6bd782d5313b4759c950fd7a53a44
Jacob Delgado <jacob.delgado@gmail.com>
2016-03-13 16:49:53 -0600

    added ability to join game

--- game/handle_request.py
+++ game/handle_request.py
@@ -5,14 +5,28 @@ from game.irefuse import Game
 
 def handle_request(json_request):
     if json_request["action"] == "START":
-        def number_of_players():
-            return json_request["players"]
-
-        game = Game()
-        try:
-            game.setup(number_of_players)
-        except AssertionError:
-            return '{ "response": 400 }'
-        return json.dumps(game, default=lambda o: o.__dict__)
+        current_game = setup_game(json_request)
+        with open("current_game.json", "w") as current:
+            current.write(current_game)
+        return current_game
+    elif json_request["action"] == "JOIN":
+        return join_game(json_request)
     else:
         return '{ "response": 400 }'
+
+
+def setup_game(json_request):
+    def number_of_players():
+        return json_request["players"]
+
+    game = Game()
+    game.setup(number_of_players)
+    return json.dumps(game, default=lambda o: o.__dict__)
+
+
+def join_game(json_request):
+    try:
+        with open("current_game.json", "r") as current_game:
+            return current_game.readlines()[-1]
+    except FileNotFoundError:
+        return '{ "response": 400, "message": "No game in progress" }'
--- unit_tests/test_handle_request.py
+++ unit_tests/test_handle_request.py
@@ -1,6 +1,8 @@
-import filecmp
+import os
 import unittest
 import json
+
+import shutil
 
 from game.handle_request import handle_request
 
@@ -19,6 +21,11 @@ def get_expected(file):
 
 class TestHandleRequest(unittest.TestCase):
     def test_handle_start_game(self):
+        try:
+            os.remove("current_game.json")
+        except FileNotFoundError:
+            pass
+
         with open(get_input("start_game")) as data_file:
             data = json.load(data_file)
             game = handle_request(data)
@@ -31,3 +38,35 @@ class TestHandleRequest(unittest.TestCase):
                 expected_json = json.load(expected)
             self.assertEquals(output_json, expected_json)
 
+    def test_handle_join_game_passes(self):
+        try:
+            os.remove("current_game.json")
+        except FileNotFoundError:
+            pass
+        shutil.copy(get_expected("start_game"), "current_game.json")
+        with open(get_input("join_game")) as data_file:
+            data = json.load(data_file)
+            game = handle_request(data)
+
+            # cards must be zeroed out since they are randomized
+            output_json = json.loads(game)
+            output_json["cards"] = []
+
+            with open(get_expected("join_game")) as expected:
+                expected_json = json.load(expected)
+            self.assertEquals(output_json, expected_json)
+
+    def test_handle_join_game_with_none_in_progress(self):
+        try:
+            os.remove("current_game.json")
+        except FileNotFoundError:
+            pass
+
+        with open(get_input("join_game_no_game_in_progress")) as data_file:
+            data = json.load(data_file)
+            game = handle_request(data)
+            output_json = json.loads(game)
+
+            with open(get_expected("join_game_no_game_in_progress")) as expected:
+                expected_json = json.load(expected)
+            self.assertEquals(output_json, expected_json)
