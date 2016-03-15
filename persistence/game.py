d00903db2811ba1e2165090a86400306488583ae
Jacob Delgado <jacob.delgado@gmail.com>
2016-03-15 00:34:58 -0600

    wip; refactoring to use persistence layer

--- persistence/game.py
+++ persistence/game.py
@@ -1,4 +1,5 @@
 # json files representing the current game
+import hashlib
 import json
 import os
 
@@ -6,33 +7,88 @@ CURRENT_GAME_JSON = "current_game.json"
 PLAYERS_JSON = "players.json"
 
 
-def create_game_record(current_game):
-    with open(CURRENT_GAME_JSON, "w") as current:
-        current.write(current_game)
+class Game(object):
+    def __init__(self):
+        pass
+
+    def record_game(self, game):
+        with open(CURRENT_GAME_JSON, "w") as current:
+            current.write(game)
+
+    def record_players(self, players):
+        with open(PLAYERS_JSON, "w") as game:
+            json.dump(players, game)
+
+    def get_players(self):
+        with open(PLAYERS_JSON, "r") as game:
+            players = json.loads(game.readlines()[0])
+        return players
+
+    def is_started(self):
+        return os.path.exists(CURRENT_GAME_JSON) or os.path.exists(PLAYERS_JSON)
+
+    def get_game_in_progress(self):
+        with open(CURRENT_GAME_JSON, "r") as game:
+            current_game = json.loads(game.readlines()[-1])
+        return current_game
+
+    def serialize_game(self, game):
+        return json.dumps(game, default=lambda o: o.__dict__)
 
 
-def serialize_players(players):
-    with open(PLAYERS_JSON, "w") as game:
-        json.dump(players, game)
+def initialize_players(json_request, number_of_players):
+    players = {}
+    for i in range(number_of_players()):
+        players[i] = None
+
+    players[0] = get_player_hash(json_request)
+
+    game = Game()
+    game.record_players(players)
 
 
-def deserialize_players():
-    with open(PLAYERS_JSON, "r") as game:
-        players = json.loads(game.readlines()[0])
-    return players
+def add_player_to_game(json_request):
+    game = Game()
+    players = game.get_players()
+
+    for player in players:
+        if players[player] is None:
+            players[player] = get_player_hash(json_request)
 
 
-def game_has_been_started():
-    return os.path.exists(CURRENT_GAME_JSON) or os.path.exists(PLAYERS_JSON)
+def get_player_hash(json_request):
+    return hashlib.md5("{}{}".format(json_request["client_ip"], json_request[
+        "client_port"]).encode("utf-8")).hexdigest()
 
 
-def get_game_in_progress():
-    with open(CURRENT_GAME_JSON, "r") as game:
-        current_game = json.loads(game.readlines()[-1])
-    return current_game
+def is_current_player(game, json_request):
+    current_game = Game()
+    players = current_game.get_players()
+
+    return players[get_current_player(game)] == get_player_hash(
+        json_request)
 
 
-def get_current_players():
-    with open(PLAYERS_JSON) as current_players:
-        players = json.loads(current_players.readlines()[0])
-    return players
+def get_current_player(game):
+    return str(game["players"]["index"])
+
+
+def has_enough_players():
+    game = Game()
+    players = game.get_players()
+
+    for i in players:
+        if players[i] is None:
+            return False
+
+    return True
+
+
+def is_player_in_game(json_request):
+    game = Game()
+    players = game.get_players()
+
+    for i in players:
+        if players[i] == get_player_hash(json_request):
+            return True
+    return False
--- restful/handle_request.py
+++ restful/handle_request.py
@@ -1,12 +1,16 @@
-import hashlib
-import json
 from abc import ABCMeta, abstractmethod
 
 from irefuse.irefuse import IRefuse
-# json responses
-from persistence.game import create_game_record, serialize_players, \
-    deserialize_players, game_has_been_started, get_game_in_progress, \
-    get_current_players
+from persistence.game import Game, initialize_players, add_player_to_game, \
+    is_current_player, has_enough_players, is_player_in_game
+
+GAME_HAS_NOT_BEEN_STARTED = '{ "response": 200, "message": "No game has been started" }'
+
+WAITING_FOR_PLAYERS = '{ "response": 200, "message": "Waiting for players" }'
+
+NOT_PLAYERS_TURN = '{ "response": 200, "player_turn": false }'
+
+PLAYERS_TURN = '{ "response": 200, "player_turn": true }'
 
 GAME_IS_CURRENTLY_IN_PROGRESS = '{ "response": 400, "message": "Game is currently in progress" }'
 GAME_IS_ALREADY_FULL_ = '{ "response": 400, "message": "Game is already full" }'
@@ -24,6 +28,9 @@ def get_request_handler(json_request):
 
 
 class RequestHandler(metaclass=ABCMeta):
+    def __init__(self):
+        self.game = Game()
+
     @abstractmethod
     def handle(self, json_request):
         pass
@@ -31,117 +38,58 @@ class RequestHandler(metaclass=ABCMeta):
 
 class StartRequestHandler(RequestHandler):
     def handle(self, json_request):
-        if game_has_been_started():
+        if self.game.is_started():
             return GAME_IS_CURRENTLY_IN_PROGRESS
 
-        return start_game(json_request)
+        return self.start_game(json_request)
+
+    def start_game(self, json_request):
+        current_game = self.setup_game(json_request)
+        self.game.record_game(current_game)
+
+        add_player_to_game(json_request)
+
+        return current_game
+
+    def setup_game(self, json_request):
+        def number_of_players():
+            return json_request["players"]
+
+        initialize_players(json_request, number_of_players)
+
+        irefuse = IRefuse()
+        irefuse.setup(number_of_players)
+        game = Game()
+        return game.serialize_game(irefuse)
 
 
 class JoinRequestHandler(RequestHandler):
     def handle(self, json_request):
-        if game_has_been_started():
-            return handle_join_after_start(json_request)
+        if self.game.is_started():
+            return self.handle_join_after_start(json_request)
         else:
             return NO_GAME_IN_PROGRESS
+
+    def handle_join_after_start(self, json_request):
+        if is_player_in_game(json_request):
+            return PLAYER_IS_ALREADY_IN_GAME
+        elif has_enough_players():
+            return GAME_IS_ALREADY_FULL_
+        else:
+            add_player_to_game(json_request)
+            return self.game.get_game_in_progress()
 
 
 class StatusRequestHandler(RequestHandler):
     def handle(self, json_request):
-        if game_has_been_started():
-            game = get_game_in_progress()
+        if self.game.is_started():
+            game = self.game.get_game_in_progress()
 
             if has_enough_players():
                 if is_current_player(game, json_request):
-                    return '{ "response": 200, "player_turn": true }'
+                    return PLAYERS_TURN
                 else:
-                    return '{ "response": 200, "player_turn": false }'
+                    return NOT_PLAYERS_TURN
             else:
-                return '{ "response": 200, "message": "Waiting for players" }'
-        return '{ "response": 200, "message": "No game has been started" }'
-
-
-def handle_join_after_start(json_request):
-    if is_player_in_game(json_request):
-        return PLAYER_IS_ALREADY_IN_GAME
-    elif has_enough_players():
-        return GAME_IS_ALREADY_FULL_
-    else:
-        add_player_to_game(json_request)
-        return get_game_in_progress()
-
-
-def start_game(json_request):
-    current_game = setup_game(json_request)
-    create_game_record(current_game)
-
-    add_player_to_game(json_request)
-
-    return current_game
-
-
-def setup_game(json_request):
-    def number_of_players():
-        return json_request["players"]
-
-    initialize_players(json_request, number_of_players)
-
-    game = IRefuse()
-    game.setup(number_of_players)
-    return serialize_game(game)
-
-
-def serialize_game(game):
-    return json.dumps(game, default=lambda o: o.__dict__)
-
-
-def initialize_players(json_request, number_of_players):
-    players = {}
-    for i in range(number_of_players()):
-        players[i] = None
-
-    players[0] = get_player_hash(json_request)
-
-    serialize_players(players)
-
-
-def add_player_to_game(json_request):
-    players = deserialize_players()
-
-    for player in players:
-        if players[player] is None:
-            players[player] = get_player_hash(json_request)
-
-
-def get_player_hash(json_request):
-    return hashlib.md5("{}{}".format(json_request["client_ip"], json_request[
-        "client_port"]).encode("utf-8")).hexdigest()
-
-
-def is_current_player(game, json_request):
-    players = get_current_players()
-
-    return players[get_current_player(game)] == get_player_hash(
-        json_request)
-
-
-def get_current_player(game):
-    return str(game["players"]["index"])
-
-
-def has_enough_players():
-    players = get_current_players()
-
-    for i in players:
-        if players[i] is None:
-            return False
-
-    return True
-
-
-def is_player_in_game(json_request):
-    players = get_current_players()
-
-    for i in players:
-        if players[i] == get_player_hash(json_request):
-            return True
-    return False
+                return WAITING_FOR_PLAYERS
+        return GAME_HAS_NOT_BEEN_STARTED
