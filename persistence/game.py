ad289432d6f79c09657c52ea0415428554d420b0
Jacob Delgado <jacob.delgado@gmail.com>
2016-03-18 21:43:56 -0600

    cleaned up licensing

--- persistence/game.py
+++ persistence/game.py
@@ -1,3 +1,22 @@
+"""
+Module is responsible for handling the persistence layer between web service
+and backend.
+Copyright (c) 2016 Jacob Delgado,
+This file is part of I Refuse.
+
+'I Refuse' is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 # json files representing the current game
 import hashlib
 import json
