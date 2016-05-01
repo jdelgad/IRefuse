6ae24a319ae6c12578196419b4fc7dcfd2fd9a68
Jacob Delgado <jacob.delgado@gmail.com>
2016-05-01 00:22:05 -0600

    Add cherrypy support and simple, dumb login support

---
+++ frontend/js/login.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    $('#failedLogin').hide();
+    $('#login').submit(function () {
+        event.preventDefault();
+        var email = $('#inputEmail').val();
+        var password = $('#inputPassword').val();
+        $.ajax({
+            url: "/login",
+            type: "POST",
+            data: JSON.stringify({email: email, password: password}),
+            dataType: "json",
+            contentType: "application/json",
+            success: function () {
+                $("#loginForm").hide();
+            },
+            error: function () {
+                $('#failedLogin').show();
+                $('input[type="email"]').css({
+                    "border": "1px solid red",
+                    "box-shadow": "0 0 3px red"
+                });
+                $('input[type="password"]').css({
+                    "border": "1px solid red",
+                    "box-shadow": "0 0 3px red"
+                });
+            }
+        });
+    });
+});
